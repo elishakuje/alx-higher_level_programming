938ae8b9b436f5037e4665db3589058ee408fb88
elishakuje <elishakuje@gmail.com>
2023-03-28 18:35:59 -0700

    3-square.py bug fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
+"""Square class to represent a square"""
+
+
 class Square:
-    def __init__(self, size=0):
-        if type(size) != int:
+    """
+    Defines a Square and its basic properties
+    >>> square_1 = Square()
+    >>> square_2 = Square(7)
+    """
+
+    def __init__(self, size=0) -> None:
+        """
+        Innitialize the size of the square. the size can be specified.
+        If they are not, the size defaults to 0
+        :param size: int size of square ( > 0)
+        """
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
 
-    def area(self):
-        return self.__size * self.__size
+    def area(self) -> int:
+        """
+        Calculates and returns the area of the square
+        :return: the area of the square
+        """
+
+        return self.__size ** 2
